67c75571925b3639a51d56f74ab1761ed25a2a5e
Katelyn <allbutanime@gmail.com>
2023-03-29 22:45:15 -0600

    added gallery and made changes to project folder

---
+++ src/components/Work.js
@@ -0,0 +1,32 @@
+import "./WorkCard.css"
+
+import WorkCard from "./WorkCard";
+import WorkCardData from "./WorkCardData";
+
+import React from 'react'
+import pro1 from "../assets/pro1.jpeg";
+import { NavLink } from "react-router-dom";
+
+const Work = () => {
+  return (
+    <div className="work-container">
+        <h1 className="project-heading">Projects</h1>
+        <div className="project-container">
+            {WorkCardData.map((val, ind) => {
+                return(
+                    <WorkCard
+                        key={ind}
+                        imgsrc={val.imgsrc}
+                        title={val.title}
+                        text={val.text}
+                        view={val.view}
+                    />
+                );
+            })}
+
+        </div>
+    </div>
+  )
+}
+
+export default Work;
